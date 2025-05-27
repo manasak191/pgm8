5424f3a303f59204bdbeaa5b6da440e08e6b6060
manasak191 <km2237548@gmail.com>
2025-05-27 21:35:58 +0530

    first commit

---
+++ pgm8/src/main/java/com/manasak191/App.java
@@ -0,0 +1,13 @@
+package com.manasak191;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
